8233d1621b72f8716eec5fdeff13e847fad3664a
Nikhil Nikhil <nikhilthakur050@gmail.com>
2019-06-19 18:36:56 -0400

    Initial Commit

---
+++ ClousersDay10/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ClousersDay10
+//
+//  Created by MacStudent on 2019-06-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
